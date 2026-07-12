a068df8364da4bbca4b4f55bbee988f3a752eec4
Bryan <cktnhat1234@gmail.com>
2026-07-12 11:01:40 -0400

    springboot API app

--- src/main/java/com/bryan/Application.java
+++ src/main/java/com/bryan/Application.java
@@ -2,12 +2,23 @@ package com.bryan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@EnableCaching
+@RequestMapping("/")
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
+    @GetMapping
+    public String helloWorld() {
+        return "Hello World";
+    }
 }
