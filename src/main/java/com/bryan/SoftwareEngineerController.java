4aee7aca17f7a09ee9935b0f64520624a6d9671c
Bryan <cktnhat1234@gmail.com>
2026-07-12 15:21:32 -0400

    add requestParams

--- src/main/java/com/bryan/SoftwareEngineerController.java
+++ src/main/java/com/bryan/SoftwareEngineerController.java
@@ -15,7 +15,15 @@ public class SoftwareEngineerController {
 
     @GetMapping
     public List<SoftwareEngineer> getEngineers() {
+        System.out.println("DEBUG: Called getEngineers() [ALL]");
         return softwareEngineerService.getAllSoftwareEngineers();
+    }
+
+    // This method fetches software engineers by their technology stack
+    @GetMapping(params = "techStack")
+    public List<SoftwareEngineer> getEngineersByTechStack(@RequestParam String techStack) {
+        System.out.println("DEBUG: Called getEngineersByTechStack() with: " + techStack);
+        return softwareEngineerService.getEngineersByTechStack(techStack);
     }
 
     @PostMapping
